104bb7bbd01bb860518bf123c903860ad4c3d511
ishiko <ishiko732@gmail.com>
2025-02-22 15:12:15 +0800

    fix api

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -5,10 +5,13 @@ import PostApp from '@/service/controllers/posts'
 
 export const runtime = 'edge'
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath('/api').notFound(async (c) => {
+  return c.json({ error: 'Not found' }, 404)
+})
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const routes = app.route('/posts', PostApp)
 
-export default handle(app)
+const handler = handle(app)
+export { handler as DELETE,handler as GET, handler as POST, handler as PUT }
 export type AppType = typeof routes
