b09ab3ddb3715df70853cc8bc94444b49713fc41
ishiko <ishiko732@gmail.com>
2025-02-25 00:02:17 +0800

    fix

--- service/controllers/train.ts
+++ service/controllers/train.ts
@@ -10,7 +10,7 @@ const formData = z.object({
 
 const TrainApp = new Hono().post('/', zValidator('form', formData), async (c) => {
   const { file } = c.req.valid('form')
-  const result = await analyzeCSV(await file.text())
+  const result = await analyzeCSV(await file.text(), 'Asia/Shanghai', 4)
   const train = await trainTask(true, result.fsrs_items)
   return c.json({ train }, 200)
 })
