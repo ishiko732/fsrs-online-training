ee26ddd39099037c5019800d9e3906a6c4933ad4
ishiko <ishiko732@gmail.com>
2025-03-20 09:56:47 +0000

    fix postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -11,6 +11,11 @@ const wasmBase64 = wasmBuffer.toString('base64')
 // 目标文件路径
 const outputPath = path.join(__dirname, 'service', 'services', 'wasm.ts')
 
+const dirPath = path.dirname(outputPath)
+if (!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath, { recursive: true })
+}
+
 // 生成 Base64 字符串并写入到 wasm.ts 文件
 const wasmTsContent = `
 export const fsrsBrowserWasmBase64 = '${wasmBase64}'
@@ -32,6 +37,11 @@ console.log(`Successfully converted fsrs_browser_bg.wasm to Base64 and wrote to 
 
 const timezones = Intl.supportedValuesOf('timeZone')
 const timeZoneOutputPath = path.join(__dirname, 'components', 'lib', 'timezones.ts')
+const timeZoneOutputDirPath = path.dirname(timeZoneOutputPath)
+if (!fs.existsSync(timeZoneOutputDirPath)) {
+  fs.mkdirSync(timeZoneOutputDirPath, { recursive: true })
+}
+
 const timezonesContent = `
 // https://stackoverflow.com/questions/38399465/how-to-get-list-of-all-timezones-in-javascript
 // issues: https://github.com/ishiko732/fsrs-online-training/issues/4
