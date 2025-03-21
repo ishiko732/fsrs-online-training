c0b1e4cc9556fa417eab762c57d11dfa4067bdd0
ishiko <ishiko732@gmail.com>
2025-03-21 20:10:17 +0800

    use sse in train method

--- service/controllers/train.ts
+++ service/controllers/train.ts
@@ -2,15 +2,29 @@ import { Readable } from 'node:stream'
 
 import { analyzeCSV } from '@api/services/collect'
 import { trainTask } from '@api/services/train_task'
+import { ProgressValue } from '@api/services/types'
 import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
+import { streamSSE } from 'hono/streaming'
 import { z } from 'zod'
 
 const formData = z.object({
   file: z.instanceof(File),
   timezone: z.string().optional().default('Asia/Shanghai'),
-  hour_offset: z.number().min(0).max(23).default(4),
-  sse: z.boolean().optional().default(false),
+  enableShortTerm: z
+    .string()
+    .transform((v) => !!v)
+    .optional()
+    .default('1'),
+  hour_offset: z
+    .string()
+    .optional()
+    .transform((v) => Math.max(23, Math.min(0, Number(v ?? 4)))),
+  sse: z
+    .string()
+    .transform((v) => !!v)
+    .optional()
+    .default('1'),
 })
 
 const TrainApp = new Hono().post('/', zValidator('form', formData), async (c) => {
@@ -21,8 +35,38 @@ const TrainApp = new Hono().post('/', zValidator('form', formData), async (c) =>
 
   const stream = Readable.from(buffer)
   const result = await analyzeCSV(stream, formData.timezone, formData.hour_offset)
-  const train = await trainTask(true, result.fsrs_items)
-  return c.json({ train }, 200)
+
+  const sseEnabled = formData.sse
+  if (!sseEnabled) {
+    const train = await trainTask(true, result.fsrs_items)
+    return c.json({ train }, 200)
+  }
+  return streamSSE(c, async (stream) => {
+    async function progress(enableShortTerm: boolean, err: Error | null, progressValue: ProgressValue) {
+      if (err) {
+        console.error(`1[enableShortTerm=${enableShortTerm}] Progress callback error:`, err)
+        return
+      }
+      await stream.writeSSE({
+        data: JSON.stringify(progressValue),
+        event: 'progress',
+        id: `${enableShortTerm ? 'short' : 'long'}-term-${progressValue.percent}`,
+      })
+      console.log(`2[enableShortTerm=${enableShortTerm}] progress value`, progressValue)
+    }
+
+    const train = await trainTask(true, result.fsrs_items, progress)
+
+    stream.onAbort(() => {
+      console.log('Aborted!')
+    })
+
+    await stream.writeSSE({
+      data: JSON.stringify(JSON.stringify(train)),
+      event: 'done',
+      id: `done`,
+    })
+  })
 })
 
 export default TrainApp
--- service/services/train_task.ts
+++ service/services/train_task.ts
@@ -4,15 +4,9 @@ import { FSRS, FSRSItem, FSRSReview } from 'fsrs-rs-nodejs'
 
 import { FSRSItem as BasicFSRSItem, ProgressValue } from './types'
 
-async function computeParametersWrapper(enableShortTerm: boolean, fsrsItems: FSRSItem[]) {
-  // create FSRS instance and optimize
-  const fsrs = new FSRS(null)
+type ProgressFunction = (enableShortTerm: boolean, err: Error | null, progressValue: ProgressValue) => void
 
-  const optimizedParameters = await fsrs.computeParameters(fsrsItems, enableShortTerm, progress.bind(null, enableShortTerm), 1000 /** 1s */)
-  return optimizedParameters
-}
-
-function progress(enableShortTerm: boolean, err: Error | null, progressValue: ProgressValue) {
+function basicProgress(enableShortTerm: boolean, err: Error | null, progressValue: ProgressValue) {
   if (err) {
     console.error(`[enableShortTerm=${enableShortTerm}] Progress callback error:`, err)
     return
@@ -20,9 +14,17 @@ function progress(enableShortTerm: boolean, err: Error | null, progressValue: Pr
   console.log(`[enableShortTerm=${enableShortTerm}] progress value`, progressValue)
 }
 
-export async function trainTask(enableShortTerm: boolean, fsrsItems: BasicFSRSItem[]) {
+async function computeParametersWrapper(enableShortTerm: boolean, fsrsItems: FSRSItem[], progress: ProgressFunction) {
+  // create FSRS instance and optimize
+  const fsrs = new FSRS(null)
+
+  const optimizedParameters = await fsrs.computeParameters(fsrsItems, enableShortTerm, progress.bind(null, enableShortTerm), 1000 /** 1s */)
+  return optimizedParameters
+}
+
+export async function trainTask(enableShortTerm: boolean, fsrsItems: BasicFSRSItem[], progress: ProgressFunction = basicProgress) {
   const fsrs_items = fsrsItems.map(
     (item: BasicFSRSItem) => new FSRSItem(item.map((review) => new FSRSReview(review.rating, review.deltaT))),
   )
-  return computeParametersWrapper(enableShortTerm, fsrs_items)
+  return computeParametersWrapper(enableShortTerm, fsrs_items, progress)
 }
